df677c43d7bd0216f600fb34e6322c89b310380e
yzero12 <2358702934@qq.com>
2023-03-16 19:34:10 +0800

    Initial commit

---
+++ src/main/java/com/yzero/java/test/Test1.java
@@ -0,0 +1,26 @@
+package com.yzero.java.test;
+
+import org.junit.Test;
+
+/**
+ * ClassName: Test1
+ * Package: com.yzero.java
+ * Description:
+ *
+ * @author zjzstart
+ * @create 2023-03-15 19:51
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    @Test
+    public void test1(){
+
+
+
+    }
+}
